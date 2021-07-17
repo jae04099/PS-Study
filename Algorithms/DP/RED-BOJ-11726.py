72d012a9102e56b3b5293c7b4ace25eef9049286
kmh339 <kmh339@gmail.com>
2021-07-17 17:34:30 +0900

    solve 11726

---
+++ Algorithms/DP/RED-BOJ-11726.py
@@ -0,0 +1,26 @@
+n = 9
+
+#import itertools
+import operator as op
+from functools import reduce
+
+def nCr(n, r):
+    r = min(r, n-r)
+    numerator = reduce(op.mul, range(n, n-r, -1), 1)
+    denominator = reduce(op.mul, range(1, r+1), 1)
+    return numerator // denominator
+
+dp = []
+
+for i in range(n+1):
+    if i == 0:
+        dp.append(1)
+        continue
+    if i%2 == 0:
+        dp.append(nCr(int(i/2 + n - i), int(i/2)))
+        # vertical = n - i
+        #dp.append(len(list(itertools.combinations(range(1, int(i/2 + n - i)+ 1), int(i/2)))))
+
+print(dp)
+
+print(sum(dp)%10007)
